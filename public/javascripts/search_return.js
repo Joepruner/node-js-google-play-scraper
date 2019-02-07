6c8aaff09707b57989361c3a0c5b7ebfe8dfdd53
Joseph Pruner <joepruner@gmail.com>
2019-02-07 10:11:02 -0800

    Can read app title from CSV, and search for a single app title and return 1 page of reviews

--- public/javascripts/search_return.js
+++ public/javascripts/search_return.js
@@ -2,79 +2,102 @@ const gplay = require('google-play-scraper');
 const fs = require('fs');
 const csv = require('csv-parser');
 const Json2csvParser = require('json2csv').Parser;
-const Json2csvTransform = require('json2csv').Transform;
-// var app_titles = {};
-// const fields = ['title'];
+const csvtojson = require('csvtojson')
 
-const fields = ['title', 'appId','score'];
-const opts = { fields };
-const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
+const fields = ['url', 'text', 'userName', 'date', 'score', 'scoreText'];
 
-const json2csvParser = new Json2csvParser({opts});
-const json2csv = new Json2csvTransform(opts, transformOpts);
+const json2csvParserTitles = new Json2csvParser();
+const json2csvParserApps = new Json2csvParser();
+const json2csvParserReviews = new Json2csvParser({ fields });
+
 
 var app_titles = [];
 var apps = {};
-var input_path = '../../input_data/worst_app_titles.csv';
-var input_stream = fs.createReadStream(input_path,{ encoding: 'utf8' });
-var output_path = '../../output_data/detailed_worst_apps.csv';
-var output_stream = fs.createWriteStream(output_path,{ encoding: 'utf8' });
+var app_Ids = [];
+// var title;
 
 
 
-input_stream
-    .pipe(csv())
-    .on('data', (row) => {
-        app_titles.push(row);
-    }).on('end', function () {
+// var titles_input_path = '../../input_data/SHORT_TEST_worst_app_titles.csv';
+var titles_input_path = '../../input_data/worst_app_titles.csv';
+var apps_output_path = '../../output_data/detailed_worst_apps.csv';
+var reviews_output_path = '../../output_data/reviews_worst_apps.csv';
 
-        var count = 1;
-        app_titles.forEach(title => {
-            // console.log(title['title']);
-            try {
-                gplay.search({
-                    term: title['title'],
-                    num: 2,
-                    fullDetail: false,
-                    throttle: 10
-                }).then(function (result) {
-                    apps = json2csvParser.parse(result);
-                        // console.log(apps);
+var titles_input_stream = fs.createReadStream(titles_input_path, { encoding: 'utf8' });
+var apps_output_stream = fs.createWriteStream(apps_output_path, { encoding: 'utf8' });
+var reviews_output_stream = fs.createWriteStream(reviews_output_path, { encoding: 'utf8' });
+// , flags: 'a'
 
 
-                    // });
-                }).then(function(){
-                    output_stream.write(apps);
-                    // console.log(apps);
+// titles_input_stream
+//     .pipe(csv())
+//     .on('data', (row) => {
+//         // console.log(row);
+//         app_titles.push(row);
+//     }).on('end', () => {
+//         console.log('Finished');
+//         titles_input_stream.destroy();
+//     });
+// return row;
+// row.age = 'forever';
+// console.log(row);
+// app_titles.forEach(title => {
+// console.log(row);
+
+csvtojson()
+    .fromFile(titles_input_path)
+    .then((titles) => {
+        // console.log(titles);
+
+        // console.log(app_titles);
+
+        for (var t in titles) {
+            app_titles.push(titles[t]);
+        }
+        return app_titles;
+    }).then((result) => {
+        // var t = titles[t];
+        // console.log(result[0]);
+        var i = 0;
+        // for (i = 0; i < 2;) {
+            // console.log(result[i]['title']);
+            // i = i-1;
+            // console.log(i);
+            // var iter = i;
+            // console.log('first loop= ' + i);
+            gplay.search({
+                term: result[i]['title'],
+                num: 1,
+                fullDetail: false,
+                throttle: 10
+            }).then((apps) => {
+                // i = i-1;
+                // console.log('second loop= ' + i);
+
+                for (var id in apps) {
+                    app_Ids.push(apps[0]['appId']);
+                }
+                // console.log(apps[0]['appId']);
+                // console.log(apps);
+                // apps = json2csvParserApps.parse(apps[0]);
+                // apps_output_stream.write(apps);
+
+                return apps;
+            }).then((result) => {
+                // console.log(result[0]['appId']);
+                // console.log('third loop= ' + i);
+                gplay.reviews({
+                    appId: result[0]['appId'],
+                    page: 0,
+                    sort: gplay.sort.NEWEST
+                }).then((reviews) => {
+                    // console.log('fourth loop= ' + i);
+                    console.log(reviews);
+                    // csv_reviews = json2csvParserReviews.parse(reviews);
+                    // reviews_output_stream.write(csv_reviews);
+
                 });
-            }
-            catch (error) {
-                console.log(error);
 
-
-
-            }
-            // console.log(csv);
-            // console.log(count);
-        });
-    }).on('end', function(){
-        console.log('Done');
+            });
+        // }
     });
-// console.log(apps);
-
-
-
-
-        // csv_output = json2csvParser.parse(apps);
-        // apps.forEach(info =>{
-        //     console.log(info);
-        // });
-        // console.log(apps);
-        // csv_output = json2csvParser.parse(apps);
-        // console.log(apps);
-
-// console.log(app_titles);
-
-
-
-
